9644f7901bc23dd1029cf3a5cc9f489f5183698b
Artem Golovko <artem.golovko@siemens.com>
2020-12-07 23:09:21 +0300

    add jar package configuration

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,24 @@
 import a0.A0Coder
+import a0.A0Decoder
 import java.io.FileInputStream
 import java.io.FileOutputStream
+import kotlin.system.exitProcess
 
-fun main() {
-  val input = FileInputStream("/home/ct/projects/data-compressor/lecture4.pdf").buffered()
-  val output = FileOutputStream("/home/ct/projects/data-compressor/encoded.pdf").buffered()
-  A0Coder.encode(input, output)
+fun main(args: Array<String>) {
+  if (args.size != 3) {
+    invalidArgsError()
+  }
+  val type = args[0]
+  val input = FileInputStream(args[1]).buffered()
+  val output = FileOutputStream(args[2]).buffered()
+  when (type) {
+    "encode" -> A0Coder.encode(input, output)
+    "decode" -> A0Decoder.decode(input, output)
+    else -> invalidArgsError()
+  }
+}
+
+private fun invalidArgsError() {
+  println("Usage: java -jar data-compressor.jar <encode | decode> <input file> <output file>")
+  exitProcess(1)
 }
