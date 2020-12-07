8d1575b3bebd10ff01773bb934bf1a6e7d612a13
Artem Golovko <artem.golovko@siemens.com>
2020-12-07 18:46:28 +0300

    implement BitOutputStream

---
+++ src/main/kotlin/a0/BitOutputStream.kt
@@ -0,0 +1,29 @@
+package a0
+
+import java.io.Closeable
+import java.io.OutputStream
+
+class BitOutputStream(
+  private val output: OutputStream
+) : Closeable {
+
+  private var currentByte = 0
+
+  private var numBitsFilled = 0
+
+  fun write(bit: Int) {
+    require(bit == 0 || bit == 1) { "Argument must be 0 or 1" }
+    currentByte = (currentByte shl 1) or bit
+    numBitsFilled++
+    if (numBitsFilled == Byte.SIZE_BITS) {
+      output.write(currentByte)
+      currentByte = 0
+      numBitsFilled = 0
+    }
+  }
+
+  override fun close() {
+    while (numBitsFilled != 0) write(0)
+    output.close()
+  }
+}
