685ae718764869bbbc51a61b0958dfdbf4087406
Artem Golovko <artem.golovko@siemens.com>
2020-12-25 06:02:08 +0300

    jpeg: a0: use different frequency models for components and DC and AC coefficients

--- src/main/kotlin/jpeg/transcoder/entropy/coding/a0/A0JpegEntropyCoder.kt
+++ src/main/kotlin/jpeg/transcoder/entropy/coding/a0/A0JpegEntropyCoder.kt
@@ -5,6 +5,7 @@ import com.davidjohnburrowes.format.jpeg.JpegData
 import com.davidjohnburrowes.format.jpeg.data.EntropyData
 import commons.*
 import jpeg.transcoder.entropy.coding.JpegEntropyCoder
+import jpeg.transcoder.entropy.coding.huffman.HuffmanJpegMeta
 import java.io.ByteArrayOutputStream
 import java.io.InputStream
 import java.io.OutputStream
@@ -22,28 +23,52 @@ private class A0JpegEntropyCoderWriter(
 
   private val jpegData = JpegData().apply { read(input) }
 
-  private val encodedSymbols = ByteArrayOutputStream()
-
-  private val coefData = ByteArrayOutputStream()
+  private val jpegMeta = HuffmanJpegMeta.fromJpeg(jpegData)
 
   private val entropyData = jpegData.filterIsInstance<EntropyData>().first()
 
   private val bitInputStream = BitInputStream(entropyData.data.inputStream())
 
+  private val componentSymbolsData =
+      mutableMapOf<Int, Pair<ByteArrayOutputStream, ByteArrayOutputStream>>()
+
+  private val coefData = ByteArrayOutputStream()
+
   private val coefDataBitOutputStream = BitOutputStream(coefData, 1)
 
   override fun writeEncoded() {
-    while (true) {
-      val symbolBits = (1..8).map { bitInputStream.read() }
-      if (symbolBits.contains(-1)) {
-        break
+    (1..jpegMeta.mcuHeight).forEach { hMcu ->
+      (1..jpegMeta.mcuWidth).forEach { vMcu ->
+        jpegMeta.components.forEach { component ->
+          val componentId = component.frameInfo.componentId
+          val componentSymbolData = componentSymbolsData
+              .getOrPut(componentId) { ByteArrayOutputStream() to ByteArrayOutputStream() }
+          (1..component.frameInfo.verticalScaling).forEach { vScale ->
+            (1..component.frameInfo.horizontalScaling).forEach { hScale ->
+              var coef = 0
+              while (coef < 64) {
+                val coefByteArray = if (coef == 0) {
+                  componentSymbolData.first
+                } else componentSymbolData.second
+                val symbolBits = (1..8).map { bitInputStream.read() }
+                val symbol = symbolBits.bitsToInt()
+                val zerosSize = symbol shr 4
+                val coefBitsSize = symbol and 15
+
+                coefByteArray.write(symbol)
+
+                if (coef != 0 && symbol == 0) {
+                  coef = 64
+                  break
+                }
+
+                coef += zerosSize + 1
+                repeat(coefBitsSize) { coefDataBitOutputStream.write(bitInputStream.read()) }
+              }
+            }
+          }
+        }
       }
-
-      val symbol = symbolBits.bitsToInt()
-      val coefBitsSize = symbol and 15
-      encodedSymbols.write(symbol)
-
-      repeat(coefBitsSize) { coefDataBitOutputStream.write(bitInputStream.read()) }
     }
     close()
   }
@@ -51,8 +76,15 @@ private class A0JpegEntropyCoderWriter(
   override fun close() {
     bitInputStream.close()
     coefDataBitOutputStream.close()
-    val symbolsEncoded = A0Compressor.encode(encodedSymbols.toByteArray())
-    entropyData.data = symbolsEncoded.size.toBytes() + symbolsEncoded + coefData.toByteArray()
+
+    val coefsByComponentsAndTable = componentSymbolsData.map {
+      val dcData = A0Compressor.encode(it.value.first.toByteArray())
+      val acData = A0Compressor.encode(it.value.second.toByteArray())
+      dcData.size.toBytes() + dcData + acData.size.toBytes() + acData
+    }.reduce { acc, bytes -> acc + bytes }
+
+    entropyData.data = coefsByComponentsAndTable + coefData.toByteArray()
+
     jpegData.write(output)
     input.close()
     output.close()
--- src/main/kotlin/jpeg/transcoder/entropy/coding/a0/A0JpegEntropyDecoder.kt
+++ src/main/kotlin/jpeg/transcoder/entropy/coding/a0/A0JpegEntropyDecoder.kt
@@ -5,6 +5,7 @@ import com.davidjohnburrowes.format.jpeg.JpegData
 import com.davidjohnburrowes.format.jpeg.data.EntropyData
 import commons.*
 import jpeg.transcoder.entropy.coding.JpegEntropyDecoder
+import jpeg.transcoder.entropy.coding.huffman.HuffmanJpegMeta
 import java.io.ByteArrayOutputStream
 import java.io.InputStream
 import java.io.OutputStream
@@ -22,6 +23,8 @@ private class A0JpegEntropyDecoderWriter(
 
   private val jpegData = JpegData().apply { read(input) }
 
+  private val jpegMeta = HuffmanJpegMeta.fromJpeg(jpegData)
+
   private val entropyData = jpegData.filterIsInstance<EntropyData>().first()
 
   private val decoded = ByteArrayOutputStream()
@@ -29,20 +32,60 @@ private class A0JpegEntropyDecoderWriter(
   private val bitOutputStream = BitOutputStream(decoded, 1)
 
   override fun writeDecoded() {
-    val symbolsSize = entropyData.data.copyOfRange(0, 4).toInt()
-    val encodedSymbols = entropyData.data.inputStream(4, symbolsSize)
-    val coefData = entropyData.data.inputStream(4 + symbolsSize, entropyData.data.size - 4 - symbolsSize)
+    var currentStreamPos = 0
+    val componentSymbolsData = jpegMeta.components.associate {
+      val componentId = it.frameInfo.componentId
+      val dcSize = entropyData.data.copyOfRange(currentStreamPos, currentStreamPos + 4).toInt()
+      currentStreamPos += 4
+      val dcSymbolsDataStream = entropyData.data.inputStream(currentStreamPos, dcSize)
+      val dcSymbolsData = A0Compressor.decode(dcSymbolsDataStream)
+      currentStreamPos += dcSize
+
+      val acSize = entropyData.data.copyOfRange(currentStreamPos, currentStreamPos + 4).toInt()
+      currentStreamPos += 4
+      val acSymbolsDataStream = entropyData.data.inputStream(currentStreamPos, acSize)
+      val acSymbolsData = A0Compressor.decode(acSymbolsDataStream)
+      currentStreamPos += acSize
+
+      componentId to (dcSymbolsData.inputStream() to acSymbolsData.inputStream())
+    }
+    val coefData = entropyData.data
+        .inputStream(currentStreamPos, entropyData.data.size - currentStreamPos)
     val coefDataBitInputStream = BitInputStream(coefData)
 
-    val decodedSymbols = A0Compressor.decode(encodedSymbols)
-    decodedSymbols.forEach { symbol ->
-      val symbolInt = symbol.toInt() and 0xFF
-      val coefBitsSize = symbolInt and 15
-      val symbolBits = symbolInt.asBitsString(8).asBits()
+    (1..jpegMeta.mcuHeight).forEach { hMcu ->
+      (1..jpegMeta.mcuWidth).forEach { vMcu ->
+        jpegMeta.components.forEach { component ->
+          val componentId = component.frameInfo.componentId
+          val componentSymbolData = componentSymbolsData.getValue(componentId)
+          (1..component.frameInfo.verticalScaling).forEach { vScale ->
+            (1..component.frameInfo.horizontalScaling).forEach { hScale ->
+              var coef = 0
+              while (coef < 64) {
+                val coefByteArray = if (coef == 0) {
+                  componentSymbolData.first
+                } else componentSymbolData.second
+                val symbol = coefByteArray.read()
+                val zerosSize = symbol shr 4
+                val coefBitsSize = symbol and 15
 
-      symbolBits.forEach { bitOutputStream.write(it) }
-      (1..coefBitsSize).forEach { bitOutputStream.write(coefDataBitInputStream.read()) }
+                val symbolBits = symbol.asBitsString(8).asBits()
+                symbolBits.forEach { bitOutputStream.write(it) }
+
+                if (coef != 0 && symbol == 0) {
+                  coef = 64
+                  break
+                }
+
+                coef += zerosSize + 1
+                repeat(coefBitsSize) { bitOutputStream.write(coefDataBitInputStream.read()) }
+              }
+            }
+          }
+        }
+      }
     }
+
     close()
   }
 
